01f6eca9fc1d05c3b126d729c16f149603d9d172
yostane <1958676+yostane@users.noreply.github.com>
2025-08-25 23:00:57 +0200

    Rename MoviesList to CountryList and update related composables

--- 02-frontend/compose-02-solution/composeApp/src/commonMain/kotlin/no/javazone/kotlinworkshop/App.kt
+++ 02-frontend/compose-02-solution/composeApp/src/commonMain/kotlin/no/javazone/kotlinworkshop/App.kt
@@ -1,23 +1,14 @@
 package no.javazone.kotlinworkshop
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import javazone2025.composeapp.generated.resources.Res
-import javazone2025.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
@@ -30,7 +21,7 @@ fun App() {
         .fillMaxSize(),
       horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-      MoviesList()
+      CountryList()
     }
   }
 }
--- 02-frontend/compose-02-solution/composeApp/src/commonMain/kotlin/no/javazone/kotlinworkshop/CountryList.kt
+++ 02-frontend/compose-02-solution/composeApp/src/commonMain/kotlin/no/javazone/kotlinworkshop/CountryList.kt
@@ -51,7 +51,7 @@ suspend fun fetchData(): List<Country> {
 }
 
 @Composable
-fun MoviesList() {
+fun CountryList() {
   var countries by remember { mutableStateOf(listOf<Country>()) }
   LaunchedEffect(Unit) {
     countries = fetchData()
@@ -79,13 +79,13 @@ fun CountryItem(country: Country) {
 
 @Preview
 @Composable
-fun MovieListPreview() {
-  MoviesList()
+fun CountryListPreview() {
+  CountryList()
 }
 
 @Preview
 @Composable
-fun MovieItemPreview() {
+fun CountryItemPreview() {
   // generate country from this json {"flags":{"png":"https://flagcdn.com/w320/no.png","svg":"https://flagcdn.com/no.svg","alt":"The flag of Norway has a red field with a large white-edged navy blue cross that extends to the edges of the field. The vertical part of this cross is offset towards the hoist side."},"name":{"common":"Norway","official":"Kingdom of Norway","nativeName":{"nno":{"official":"Kongeriket Noreg","common":"Noreg"},"nob":{"official":"Kongeriket Norge","common":"Norge"},"smi":{"official":"Norgga gonagasriika","common":"Norgga"}}},"capital":["Oslo"],"flag":"🇳🇴"}
   val country = Country(
     name = CountryName(
